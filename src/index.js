d43e810649339be17682d077735dc30c9c74f4b9
ted.phillips <ted.phillips11@btinternet.com>
2022-02-04 12:34:06 +0000

    everything linked up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import AppComponent from "./components/app/app-component.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppComponent />
   </React.StrictMode>,
   document.getElementById("root")
 );
